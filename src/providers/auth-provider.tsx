aa267e0114ea15c970978b24189cff9a08026ff6
John Crain <jcrain@MacBook-Pro-4.local>
2024-12-21 11:29:46 -0400

    Linter clean up

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -2,15 +2,16 @@
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect } from "react";
+import { AuthSession } from "@supabase/supabase-js";
 
 export function AuthProvider(): null {
   const supabase = createClientComponentClient();
 
   useEffect(() => {
-    supabase.auth.onAuthStateChange((event, session) => {
+    supabase.auth.onAuthStateChange((event, session: AuthSession | null) => {
       supabase.auth.setSession({
-        access_token: session?.access_token ?? null,
-        refresh_token: session?.refresh_token ?? null,
+        access_token: session?.access_token || undefined,
+        refresh_token: session?.refresh_token || undefined,
         options: {
           redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
         },
