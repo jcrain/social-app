65a722c6f4315f70a7128f885f39f34fe6dbfd22
John Crain <jcrain@MacBook-Pro-4.local>
2024-12-21 12:03:35 -0400

    More linting

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -2,7 +2,6 @@
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect } from "react";
-import { AuthSession, Session } from "@supabase/supabase-js";
 
 export function AuthProvider(): null {
   const supabase = createClientComponentClient();
