9692ef450f026cc9bf3ab8d3777784d60c539af0
John Crain <jcrain@MacBook-Pro-4.local>
2024-12-21 11:58:25 -0400

    Linter for new UI

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -2,20 +2,16 @@
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect } from "react";
-import { AuthSession } from "@supabase/supabase-js";
+import { AuthSession, Session } from "@supabase/supabase-js";
 
 export function AuthProvider(): null {
   const supabase = createClientComponentClient();
 
   useEffect(() => {
-    supabase.auth.onAuthStateChange((event, session: AuthSession | null) => {
-      supabase.auth.setSession({
-        access_token: session?.access_token || undefined,
-        refresh_token: session?.refresh_token || undefined,
-        options: {
-          redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
-        },
-      });
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      if (session) {
+        await supabase.auth.setSession(session);
+      }
     });
   }, [supabase.auth]);
 
